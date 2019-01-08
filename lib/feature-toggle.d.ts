745da98573d3915398061a5a24130c3701c30bbf
Michael Styve <michael.styve@T-Mobile.com>
2019-01-08 11:19:50 -0800

    Initial commit

---
+++ lib/feature-toggle.d.ts
@@ -0,0 +1,22 @@
+export interface FeatureFlagConfiguration {
+    metaData: {
+        environment: string;
+        owner: string;
+        featureExpiryWarningDays: number;
+    };
+    features: {
+        [featureName: string]: {
+            enabled: boolean;
+            descreiption: string;
+            expiry: string;
+        };
+    };
+}
+export declare class FeatureToggle {
+    private config;
+    constructor(config: FeatureFlagConfiguration);
+    isEnabled(featureName: string): boolean;
+    execFeature(featureName: string, enabledFunction: Function, disabledFunction: Function): any;
+    getWarningFeatures(): string[];
+    getExpiredFeatures(): string[];
+}
